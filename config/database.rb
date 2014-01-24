75a3d5cff8ca3255721e71bce057e19d385ff58c
rugger-sr <rugger.sr@gmail.com>
2014-01-24 20:09:52 +0900

    開発DBをローカルのpostgresに切り替え

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,8 @@
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
-  when :development then Sequel.connect("sqlite://" + Padrino.root('db', "padrino_test_development.db"), :loggers => [logger])
+#  when :development then Sequel.connect("sqlite://" + Padrino.root('db', "padrino_test_development.db"), :loggers => [logger])
+  when :development then Sequel.connect("postgres://postgres@localhost/test", :loggers => [logger])
   when :production  then Sequel.connect("sqlite://" + Padrino.root('db', "padrino_test_production.db"),  :loggers => [logger])
   when :test        then Sequel.connect("sqlite://" + Padrino.root('db', "padrino_test_test.db"),        :loggers => [logger])
 end
